3e326a339d966302445e3da4663ae0695fad9fee
IamHerica <hericacadoni@gmail.com>
2021-10-06 11:49:09 -0300

    Add project files.

---
+++ APIProduto/Data/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIProduto.Data
+{
+    public class Produto
+    {
+        //Modelo anemico; vai agir como um DTO.
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Estoque { get; set; }
+    }
+}
